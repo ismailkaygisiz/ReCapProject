863a231707ac12680ed6c20bc0ced91191d1cea4
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-02-17 18:39:45 +0300

    IoC Containers Added to Startup.cs in WebAPILayer

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,7 +31,18 @@ namespace WebAPI
         {
             services.AddControllers();
             services.AddSingleton<ICarService, CarManager>();
+            services.AddSingleton<IBrandService, BrandManager>();
+            services.AddSingleton<IColorService, ColorManager>();services.AddSingleton<ICarService, CarManager>();
+            services.AddSingleton<IUserService, UserManager>();
+            services.AddSingleton<ICustomerService, CustomerManager>();
+            services.AddSingleton<IRentalService, RentalManager>();
+            
             services.AddSingleton<ICarDal, EfCarDal>();
+            services.AddSingleton<IBrandDal, EfBrandDal>();
+            services.AddSingleton<IColorDal, EfColorDal>();
+            services.AddSingleton<IUserDal, EfUserDal>();
+            services.AddSingleton<ICustomerDal, EfCustomerDal>();
+            services.AddSingleton<IRentalDal, EfRentalDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
