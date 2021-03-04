2c674bdd773fd59aa20db27e6b9a3ac59c3c63cd
ismailkaygisiz <kaygisizismail24@gmail.com>
2021-03-04 16:38:56 +0300

    Update AspectInterceptorSelector.cs

--- Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -16,7 +16,7 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-            classAttributes.Add(new PerformanceAspect(20));
+            classAttributes.Add(new PerformanceAspect(10));
 
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
